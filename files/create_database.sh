fa1dd181a9a85df1a00dc19f4463acc13a746b2b
edgd1er <edgd1er@hotmail.com>
2020-07-05 01:22:37 +0200

    wait for db to be ready, insert schema if needed, start processes

--- files/create_database.sh
+++ files/create_database.sh
@@ -38,15 +38,33 @@ setRights(){
 
 }
 
+waitForMysql() {
+  n=0
+  while (true and ${n} -lt 30 ); do
+    /usr/bin/mysql -h ${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -D ${MYSQL_DATABASE} -P${MYSQL_HOST_PORT} -e 'show databases;'
+    ret=$?
+    [[ 0 == ${ret} ]] && echo -e "\n OK, DB is up and running \n" && break
+    echo -e "\n Error, server ${MYSQL_HOST}:${MYSQL_HOST_PORT} is not up or db ${MYSQL_DATABASE} is not accessible with credentials ${MYSQL_USER} / ${MYSQL_PASSWORD}"
+    sleep 5
+    n++;
+  done
+}
+
 ## Main
 # wait for mysql to be ready.
-sleep 30
+waitForMysql
 
-res=$(mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST -D$MYSQL_DATABASE -Be 'show tables;')
+res=$(mysql -h ${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -D ${MYSQL_DATABASE} -P${MYSQL_HOST_PORT} -Be 'show tables;')
 ret=$?
 
 if [ $ret -ne 0 -o $(echo $res | wc -w) -lt 7 ]; then
+    echo -e "\n/!\Schema is not complete/!\ "
     createDatabase
     generateMysql
 fi
 setRights
+
+echo -e "Starting apache"
+supervisorctl start apache2
+echo -e "Starting icinga"
+supervisorctl start icinga
